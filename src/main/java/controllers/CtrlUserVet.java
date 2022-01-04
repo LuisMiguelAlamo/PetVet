a15c6fb1eba8a02b62da006d01d87c824b9e5b8e
Luis Miguel <Luis Miguel@LuisMi-Escritorio>
2022-01-04 22:18:47 +0000

    se han agregado los botones de cerrar se eliminó la barra superior y se activó undecorated se creó la transcición de cliente a registro cliente y viceversa

---
+++ src/main/java/controllers/CtrlUserVet.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import javax.swing.JPanel;
+import views.ClientePanel;
+import views.ConsultasPanel;
+import views.FrmVeterinario;
+import views.MascotasPanel;
+
+/**
+ *
+ * @author Luis Miguel
+ */
+public class CtrlUserVet implements MouseListener, MouseMotionListener{
+    FrmVeterinario frm;
+    int xMouse, yMouse;
+
+    public CtrlUserVet() {
+        frm = new FrmVeterinario();
+        this.frm.getBtnClientes().addMouseListener(this);
+        this.frm.getBtnMascotas().addMouseListener(this);
+        this.frm.getBtnConsultas().addMouseListener(this);
+        this.frm.getHeaderPanel().addMouseMotionListener(this);
+        this.frm.getExitPanel().addMouseListener(this);
+        this.frm.getExitLabel().addMouseListener(this);
+        frm.setVisible(true);
+    }
+    
+    
+    public static void showContentPanel(JPanel p){
+                       
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (e.getSource().equals(this.frm.getBtnPrincipal())) {
+            ClientePanel cp = new ClientePanel();
+            showContentPanel(cp);
+        }
+        if (e.getSource().equals(this.frm.getBtnClientes())) {
+            ClientePanel cp = new ClientePanel();
+            CtrlClientes cli = new CtrlClientes(frm, cp);
+        }
+        if (e.getSource().equals(this.frm.getBtnMascotas())) {
+            MascotasPanel mp = new MascotasPanel();
+            CtrlMascotas mascotas = new CtrlMascotas(frm, mp);
+        }
+        if (e.getSource().equals(this.frm.getBtnConsultas())) {
+            ConsultasPanel cop = new ConsultasPanel();
+            showContentPanel(cop);
+        }
+        if (e.getSource().equals(this.frm.getExitLabel())) {
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if (e.getSource().equals(this.frm.getHeaderPanel())) {
+            this.xMouse = e.getX();
+            this.yMouse = e.getY();
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        if (e.getSource().equals(this.frm.getExitLabel())) {
+            this.frm.getExitPanel().setBackground(Color.red);
+            this.frm.getExitLabel().setForeground(Color.WHITE);
+        }
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        if (e.getSource().equals(this.frm.getExitLabel())) {
+            this.frm.getExitPanel().setBackground(Color.WHITE);
+            this.frm.getExitLabel().setForeground(Color.BLACK);
+        }
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        if (e.getSource().equals(this.frm.getHeaderPanel())) {
+            int x = e.getXOnScreen();
+            int y = e.getYOnScreen();
+            this.frm.setLocation(x - xMouse, y - yMouse);
+        }
+        
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+    }
+    
+    
+}
